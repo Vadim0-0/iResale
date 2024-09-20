455185a1d3e54fcb5e5cc35799de398ffd60abea
Vadim0-0 <160814841+Vadim0-0@users.noreply.github.com>
2024-09-20 22:23:23 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,45 +39,6 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const devices = document.querySelectorAll('.installmentPlan-hero__trade-devices__selector-device');
-  const movingLine = document.getElementById('trade-moving-line');
-  const formsContainer = document.querySelector('.installmentPlan-hero__trade-devices__forms');
-  const contents = document.querySelectorAll('.installmentPlan-hero__trade-devices__forms-form');
-
-  // Функция для установки динамической высоты контейнера
-  function updateContainerHeight(activeContent) {
-      const contentHeight = activeContent.offsetHeight; // Получаем высоту активного блока
-      formsContainer.style.height = `${contentHeight}px`; // Устанавливаем высоту контейнера
-  }
-
-  devices.forEach((device, index) => {
-      device.addEventListener('click', () => {
-          // Удаляем активный класс у всех устройств
-          devices.forEach(d => d.classList.remove('active'));
-          // Добавляем активный класс к нажатому устройству
-          device.classList.add('active');
-
-          // Перемещаем внутреннюю линию
-          movingLine.style.left = `${index * 50}%`; // Перемещение в процентах
-
-          // Отображаем соответствующий контент
-          contents.forEach(content => content.classList.remove('active'));
-          const targetContent = document.getElementById(device.getAttribute('data-target'));
-          targetContent.classList.add('active');
-
-          // Обновляем высоту контейнера
-          updateContainerHeight(targetContent);
-      });
-  });
-
-  // Устанавливаем начальную высоту контейнера
-  const initialActiveContent = document.querySelector('.installmentPlan-hero__trade-devices__forms-form.active');
-  if (initialActiveContent) {
-      updateContainerHeight(initialActiveContent);
-  }
-});
-
 /* Секция - частые вопросы */
 
 document.querySelectorAll('.questions__content-faq__item-header').forEach(header => {
